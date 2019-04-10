6a916e77fe28efe76091ead384e86e2a3bc29df8
Riccardo Briccola <riccardo.briccola00@gmail.com>
2019-04-10 20:51:05 +0200

    Code cleaning, attempts to connect until host is reachable

--- main.py
+++ main.py
@@ -2,10 +2,36 @@ import time
 import sys
 import platform
 import subprocess
-import random
 import psutil
+from time import strftime
+import datetime
 import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
+
+if len(sys.argv) < 3:
+    print("You must pass broker address and client name")
+    sys.exit()
+broker = sys.argv[1]
+name = sys.argv[2]
+
+main_topic = "monitor/"+name+"/"
+# Topic informazioni
+status_topic = main_topic+'status'
+ram_topic = main_topic+'ram_used_percentage'
+cpu_topic = main_topic+'cpu_used_percentage'
+disk_topic = main_topic+'disk_used_percentage'
+os_topic = main_topic+'operating_system'
+time_topic = main_topic+'message_time'
+# Topic comandi
+shutdown_topic = main_topic+"shutdown_command"
+reboot_topic = main_topic+"reboot_command"
+lock_topic = main_topic+"lock_command"
+
+#Delay in second
+message_send_delay = 10
+connection_error_delay = 5
+client_connected = False
+connection_failed = False
 
 
 def Get_RAM_Percentage():
@@ -22,6 +48,10 @@ def Get_Disk_Used_Percentage():
 
 def Get_Operating_System():
     return platform.system()
+
+
+def Get_Time():
+    return datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 
 def Shutdown():
@@ -57,6 +87,18 @@ def Lock():
         subprocess.Popen(cmdCommand.split(), stdout=subprocess.PIPE)
 
 
+def on_connect(client, userdata, flags, rc):
+    # Quando ho la connessione mi iscrivo ai topic per ricevere i comandi
+    # Inserisco qua le subscription per eseguirle anche al riavvio del server
+    global client_connected
+    if rc == 0:  # Se la connessione è riuscita
+        print("Connection established")
+        client_connected = True
+        client.subscribe(shutdown_topic)
+        client.subscribe(reboot_topic)
+        client.subscribe(lock_topic)
+
+
 def on_message(client, userdata, message):
     if message.topic == shutdown_topic:
         Shutdown()
@@ -68,49 +110,41 @@ def on_message(client, userdata, message):
         print("Unknown topic: " + message.topic)
 
 
-def on_connect(client, userdata, flags, rc):
-    # Quando ho la connessione mi iscrivo ai topic per ricevere i comandi
-    # Inserire qua le subscription per eseguirle anche al riavvio del server
-    client.subscribe(shutdown_topic)
-    client.subscribe(reboot_topic)
-    client.subscribe(lock_topic)
+def on_disconnect(client, userdata, rc):
+    global client_connected
+    print("Connection lost")
+    client_connected = False
 
 
-if len(sys.argv) < 3:
-    sys.exit()
-broker = sys.argv[1]
-name = sys.argv[2]
-main_topic = "monitor/"+name+"/"
 print("Broker: " + broker)
 print("Topic: " + main_topic)
 
-# Topic informazioni
-ram_topic = main_topic+'ram_used_percentage'
-cpu_topic = main_topic+'cpu_used_percentage'
-disk_topic = main_topic+'disk_used_percentage'
-os_topic = main_topic+'operating_system'
-# Topic comandi
-shutdown_topic = main_topic+"shutdown_command"
-reboot_topic = main_topic+"reboot_command"
-lock_topic = main_topic+"lock_command"
-
-delay = 10
 # Preparo il client
 client = mqtt.Client("monitor-" + name)
 client.on_message = on_message
 client.on_connect = on_connect
-# Mi connetto al server
-client.connect(broker)
+client.on_disconnect = on_disconnect
+
+# Mi connetto al server in modo asincrono:
+# se il server non è raggiungibile, appena sarà possibile avverrà la connessione
+client.connect_async(broker)
+
 # Inizio il loop di invio dei dati ogni {delay} secondi
 client.loop_start()
 
 while True:
-    # Invio la % della RAM usata
-    client.publish(ram_topic, Get_RAM_Percentage())
-    # Invio la % della CPU usata
-    client.publish(cpu_topic, Get_CPU_Percentage())
-    # Invio la % del disco usato
-    client.publish(disk_topic, Get_Disk_Used_Percentage())
-    # Invio il Sistema Operativo
-    client.publish(os_topic, Get_Operating_System())
-    time.sleep(delay)
+    if client_connected:
+        # Invio lo stato
+        client.publish(status_topic, "home")
+        # Invio la % della RAM usata
+        client.publish(ram_topic, Get_RAM_Percentage())
+        # Invio la % della CPU usata
+        client.publish(cpu_topic, Get_CPU_Percentage())
+        # Invio la % del disco usato
+        client.publish(disk_topic, Get_Disk_Used_Percentage())
+        # Invio il Sistema Operativo
+        client.publish(os_topic, Get_Operating_System())
+        # Invio l'ora attuale
+        client.publish(time_topic, Get_Time())
+        print(Get_Time())
+    time.sleep(message_send_delay)
