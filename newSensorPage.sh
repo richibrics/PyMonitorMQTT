3e3df23f1379604c310a401e5a7d0e88924cf494
Riccardo Briccola <riccardo.briccola00@gmail.com>
2021-01-11 14:21:14 +0100

    Bash file to add new sensor struct

---
+++ newSensorPage.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp docs/sensors/template_page.md docs/sensors/$1.md
+mkdir docs/_includes/data/sensors/$1
+mkdir docs/_includes/data/sensors/$1/example
+mkdir docs/_includes/data/sensors/$1/extra
